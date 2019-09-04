104ec9c39511ec675e0468c35ad22a801f26c5ca
yaseeniqbal <52909968+yaseeniqbal@users.noreply.github.com>
2019-09-04 16:36:15 +0500

    make foloosi currency dynamic

--- app/controllers/spree/foloosi_controller.rb
+++ app/controllers/spree/foloosi_controller.rb
@@ -102,7 +102,7 @@ module Spree
       {
         redirect_url: foloosi_v2_url,
         transaction_amount: @order.total,
-        currency: 'AED'
+        currency: current_currency.present? ? current_currency : "AED"
       }
 
     end
